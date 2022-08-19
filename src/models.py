5e09205b2d64d65729a2f90770c7e6c60c9cb56a
Sussanara <sussanaracely@gmail.com>
2022-08-19 21:19:36 +0000

    Avance

--- src/models.py
+++ src/models.py
@@ -1,2 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+Favorites_People = db.Table('Favorites_People',
+    db.Column('people_id', db.Integer, db.Foreignkey('People.id'), primary_key=True),
+    db.Column('users_id', db.Integer, db.Foreignkey('User.id'), primary_key=True)
+)
+
+Favorites_vehicles = db.Table('Favorites_vehicles',
+    db.Column('vehicles_id', db.Integer, db.Foreignkey('Vehicles.is'), primary_key=True),
+    db.Column('users_id', db.Integer, db.Foreignkey('User.id'), primary_key=True)
+)
+
+Favorites_planets = db.Table('Favorites_planets',
+    db.Column('planets_id', db.Integer, db.Foreignkey('Planets.id'), primary_key=True),
+    db.Column('users_id', db.Integer, db.Foreignkey('User.id'), primary_key=True)
+)
+
+Class User(db.Model):
+    _tablename_='User'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column
