b3e98416a87b3d036d12b185e373bfebfd97b022
Sussanara <sussanaracely@gmail.com>
2022-08-19 22:14:29 +0000

    avance2

--- src/models.py
+++ src/models.py
@@ -1,7 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-Favorites_People = db.Table('Favorites_People',
+Class User(db.Model):
+    _tablename_='User'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False,)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    people = db.relationship('People', secondary="Favorites_people")
+    planets = db.relationship('Planets', secondary="Favoreites_planets")
+    vehicles = db.relationship('Vehicles', secondary="Favorites_vehicles")
+
+
+Favorites_People = db.Table('Favorites_people',
     db.Column('people_id', db.Integer, db.Foreignkey('People.id'), primary_key=True),
     db.Column('users_id', db.Integer, db.Foreignkey('User.id'), primary_key=True)
 )
@@ -16,7 +27,8 @@ Favorites_planets = db.Table('Favorites_planets',
     db.Column('users_id', db.Integer, db.Foreignkey('User.id'), primary_key=True)
 )
 
-Class User(db.Model):
-    _tablename_='User'
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column
+def serialize(self):
+    return {
+        "id": self.id,
+        "username": self.username
+    }
